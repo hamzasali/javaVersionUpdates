67067255143120c2bee63b69d5ed4ad7d1f493a0
Hamza Ali <hamzamughal2145@gmail.com>
2022-08-15 12:26:22 -0400

    Genetics

---
+++ m1-genetics/src/main/java/com/cydeo/Main.java
@@ -0,0 +1,45 @@
+package com.cydeo;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> items = new ArrayList<Integer>();  //ArrayList only accepts objects belongs to Integer class
+
+        items.add(1);
+        items.add(2);
+        items.add(3);
+        items.add(4);
+//        items.add("Apple");
+
+
+        printDoubled(items);
+
+        System.out.println("================================================");
+
+        FootballPlayer joe = new FootballPlayer("Joe");
+        BaseballPlayer pat = new BaseballPlayer("pat");
+        SoccerPlayer beckham = new SoccerPlayer("Beckham");
+
+        Team<SoccerPlayer> liverpool = new Team("Liverpool");
+
+//        liverpool.addPlayer(joe);
+//        liverpool.addPlayer(pat);
+        liverpool.addPlayer(beckham);
+
+        /*cannot be any other class than Player or sub of Player
+
+        Team<String> brokenTeam = new Team<>("what team is this?");
+        Team<Integer> t2 = new Team<>("Numbers");*/
+
+
+    }
+
+    private static void printDoubled(ArrayList<Integer> items) {
+        for (Integer i : items) {
+            System.out.println(i * 2);
+        }
+    }
+}
