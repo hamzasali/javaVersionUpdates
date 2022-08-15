dd9c201b27764d1518c71f4346b58941b9fabd52
Hamza Ali <hamzamughal2145@gmail.com>
2022-08-15 13:09:09 -0400

    Lambda

--- m3-lambda/src/main/java/com/cydeo/Apple.java
+++ m3-lambda/src/main/java/com/cydeo/Apple.java
@@ -1,37 +1,20 @@
 package com.cydeo;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+
 public class Apple {
 
     private Color appleColor;
 
     private Double appleWeight;
 
-    public Apple(Color appleColor, Double appleWeight) {
-        this.appleColor = appleColor;
-        this.appleWeight = appleWeight;
-    }
 
-    public Color getAppleColor() {
-        return appleColor;
-    }
-
-    public void setAppleColor(Color appleColor) {
-        this.appleColor = appleColor;
-    }
-
-    public Double getAppleWeight() {
-        return appleWeight;
-    }
-
-    public void setAppleWeight(Double appleWeight) {
-        this.appleWeight = appleWeight;
-    }
-
-    @Override
-    public String toString() {
-        return "Apple{" +
-                "appleColor=" + appleColor +
-                ", appleWeight=" + appleWeight +
-                '}';
-    }
 }
