68f56fad6254b21d47ef2054088aa5d02cda6664
Hamza Ali <hamzamughal2145@gmail.com>
2022-08-23 15:14:24 -0400

    Java 14

---
+++ m8-new-version-updates/src/main/java/com/cydeo/java14/UserTest.java
@@ -0,0 +1,10 @@
+package com.cydeo.java14;
+
+public class UserTest {
+    public static void main(String[] args) {
+        User user = new User("mike", "123 ave a ");
+        System.out.println(user.name());
+        System.out.println(user);
+
+    }
+}
