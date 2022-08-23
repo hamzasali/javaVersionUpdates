7ba93f3089972562d19568d0aa3357ac75c80244
Hamza Ali <hamzamughal2145@gmail.com>
2022-08-23 12:40:43 -0400

    Java 10

---
+++ m8-new-version-updates/src/main/java/com/cydeo/java10/LocalVariableTypeDemo.java
@@ -0,0 +1,23 @@
+package com.cydeo.java10;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class LocalVariableTypeDemo {
+    public static void main(String[] args) {
+
+        var str = "Cydeo";
+        var num = 1;
+
+        byte x = 1;
+        var y = 1; // int
+
+        var price = 12.23; // double
+
+        var numbers = Arrays.asList(2,3,4,5);
+
+
+
+
+    }
+}
